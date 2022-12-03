96b1b2b4992e2f9327917d85e9faaac2c02c16d7
IgorScarpari <101305117+IgorScarpari@users.noreply.github.com>
2022-12-03 19:30:41 -0300

    Incluído novas rotas nas reservas

--- routes/reserve.js
+++ routes/reserve.js
@@ -6,6 +6,8 @@ const reserveController = require('../controllers/reserve');
 router.get("/", reserveController.findAll);
 router.get("/:id", reserveController.findOne);
 router.get("/bookuser/:bookId&:userId", reserveController.findByUserOrBook);
+router.get("/book/:bookId", reserveController.findByBook);
+router.get("/user/:userId", reserveController.findByUser);
 router.post("/", reserveController.create);
 router.put("/:id", reserveController.update);
 router.delete("/:id", reserveController.delete);
