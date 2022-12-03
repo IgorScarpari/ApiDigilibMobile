5a588d4d5c837bc10b2e9a070b60d11e6eb9e3c2
IgorScarpari <101305117+IgorScarpari@users.noreply.github.com>
2022-12-03 16:45:16 -0300

    Update reserve.js

--- routes/reserve.js
+++ routes/reserve.js
@@ -5,7 +5,7 @@ const reserveController = require('../controllers/reserve');
 //add routers
 router.get("/", reserveController.findAll);
 router.get("/:id", reserveController.findOne);
-router.get("/:bookId&:userId", reserveController.findByUserOrBook);
+router.get("/bookuser/:bookId&:userId", reserveController.findByUserOrBook);
 router.post("/", reserveController.create);
 router.put("/:id", reserveController.update);
 router.delete("/:id", reserveController.delete);
