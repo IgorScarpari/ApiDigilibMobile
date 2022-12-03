ac6a6472d87bc7c5821bc8a3bc2ded301b7efe77
IgorScarpari <101305117+IgorScarpari@users.noreply.github.com>
2022-12-03 15:29:41 -0300

    Update reserve.js
    Incluído nova rota

--- routes/reserve.js
+++ routes/reserve.js
@@ -5,6 +5,7 @@ const reserveController = require('../controllers/reserve');
 //add routers
 router.get("/", reserveController.findAll);
 router.get("/:id", reserveController.findOne);
+router.get("/:bookId&:userId", reserveController.findByUserOrBook);
 router.post("/", reserveController.create);
 router.put("/:id", reserveController.update);
 router.delete("/:id", reserveController.delete);
