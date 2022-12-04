8b6b3f9796c55ae6ad9abc19c8d9b4839928c7c4
IgorScarpari <101305117+IgorScarpari@users.noreply.github.com>
2022-12-04 14:32:01 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,42 +1,54 @@
-const Sequelize = require('sequelize');
-const database = require('../db');
+"use strict";
 
-module.exports = database.sequelize.define('Users', {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true
-    },
-    registration: {
-      type: Sequelize.INTEGER,
-      required: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      required: true,
-    },
-    lastName: Sequelize.STRING,
-    email: Sequelize.STRING,
-    phoneNumber: Sequelize.STRING,
-    acessGroup: {
-      type: Sequelize.ENUM("Admin", "Professor", "Aluno"),
-      default: "Aluno",
-      required: false
-    },
-    userImage: { 
-      type: Sequelize.BLOB('long'), // <- type for image ( database :postgresql )
-      allowNull: true 
-    },
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE,
-  }, {
-    modelName: 'Users',
-    updatedAt: 'updatedAt',
-    createdAt: 'createdAt',
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
-      }
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      models.User.hasMany(models.Reserve, {
+        foreignKey: "userId",
+      });
     }
-  });
+  }
+
+  User.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      registration: {
+        type: Sequelize.INTEGER,
+        required: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        required: true,
+      },
+      lastName: Sequelize.STRING,
+      email: Sequelize.STRING,
+      phoneNumber: Sequelize.STRING,
+      acessGroup: {
+        type: Sequelize.ENUM("Admin", "Professor", "Aluno"),
+        default: "Aluno",
+        required: false
+      },
+      userImage: { 
+        type: Sequelize.BLOB('long'), // <- type for image ( database :postgresql )
+        allowNull: true 
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    },
+    {
+      sequelize,
+      modelName: "Users",
+      updatedAt: 'updatedAt',
+      createdAt: 'createdAt',
+    }
+  );
+
+  return User;
+};
