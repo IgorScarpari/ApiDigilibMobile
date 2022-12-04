a1037bde390df0552f6b20c1bf80e1cd9839bbcc
IgorScarpari <101305117+IgorScarpari@users.noreply.github.com>
2022-12-04 14:31:32 -0300

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,46 +1,63 @@
-const Sequelize = require('sequelize');
-const database = require('../db');
+"use strict";
 
-module.exports = database.sequelize.define('Books', {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true
-    },
-    internalCode: Sequelize.INTEGER,
-    isbn: Sequelize.STRING,
-    title: {
-      type: Sequelize.STRING,
-      required: true,
-    },
-    subtitle: Sequelize.STRING,
-    genre: Sequelize.STRING,
-    volume: Sequelize.STRING,
-    edition: Sequelize.STRING,
-    collection: Sequelize.STRING,
-    language: Sequelize.STRING,
-    synopsis: Sequelize.STRING,
-    originCountry: Sequelize.STRING,
-    author: Sequelize.STRING,
-    authorLastName: Sequelize.STRING,
-    publishingCompany: Sequelize.STRING,
-    publishDate: Sequelize.DATE,
-    pages: Sequelize.INTEGER,
-    ageGroup: Sequelize.INTEGER,
-    bookImage: { 
-      type: Sequelize.BLOB('long'), // <- type for image ( database :postgresql )
-      allowNull: true 
-    },
-    bookSituation: {
-      type: Sequelize.ENUM("Livre", "Emprestado", "Perdido", "Extraviado"),
-      default: "Livre",
-      required: false
-    },
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE,
-  }, {
-    modelName: 'Books',
-    updatedAt: 'updatedAt',
-    createdAt: 'createdAt',
-  });
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    class Book extends Model {
+        static associate(models) {
+            models.Book.hasMany(models.Reserve, {
+                foreignKey: "bookId",
+            });
+        }
+    }
+
+    Book.init(
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+          },
+          internalCode: Sequelize.INTEGER,
+          isbn: Sequelize.STRING,
+          title: {
+            type: Sequelize.STRING,
+            required: true,
+          },
+          subtitle: Sequelize.STRING,
+          genre: Sequelize.STRING,
+          volume: Sequelize.STRING,
+          edition: Sequelize.STRING,
+          collection: Sequelize.STRING,
+          language: Sequelize.STRING,
+          synopsis: Sequelize.STRING,
+          originCountry: Sequelize.STRING,
+          author: Sequelize.STRING,
+          authorLastName: Sequelize.STRING,
+          publishingCompany: Sequelize.STRING,
+          publishDate: Sequelize.DATE,
+          pages: Sequelize.INTEGER,
+          ageGroup: Sequelize.INTEGER,
+          bookImage: { 
+            type: Sequelize.BLOB('long'), // <- type for image ( database :postgresql )
+            allowNull: true 
+          },
+          bookSituation: {
+            type: Sequelize.ENUM("Livre", "Emprestado", "Perdido", "Extraviado"),
+            default: "Livre",
+            required: false
+          },
+          createdAt: Sequelize.DATE,
+          updatedAt: Sequelize.DATE,
+        },
+        {
+            sequelize,
+            modelName: "Books",
+            updatedAt: 'updatedAt',
+            createdAt: 'createdAt',
+        }
+    );
+
+    return Book;
+};
